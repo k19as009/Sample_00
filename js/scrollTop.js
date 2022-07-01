1591e43f71e206ff5941a78793bc8210dcfe788a
Kino <84436151+k19as009@users.noreply.github.com>
2022-07-02 02:25:04 +0900

    Delete js directory

--- js/scrollTop.js
+++
@@ -1,7 +0,0 @@
-jQuery(window).on('scroll', function () {
-　if (7000 < jQuery(this).scrollTop()) {
-　　jQuery('.header').addClass('change-color');
-　} else {
-　　jQuery('.header').removeClass('change-color');
-　}
-});
