da956243c700b615d9a1d60cf9e4d82e75d52c66
Kino <84436151+k19as009@users.noreply.github.com>
2022-07-02 02:24:12 +0900

    Delete Java directory

--- Java/scrollTop.js
+++
@@ -1,7 +0,0 @@
-jQuery(window).on('scroll', function () {
-　if (7000 < jQuery(this).scrollTop()) {
-　　jQuery('#box9').addClass('change-color');
-　} else {
-　　jQuery('#box9').removeClass('change-color');
-　}
-});
